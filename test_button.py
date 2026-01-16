df1c5cdb2281f7edb783ecd5a7aceb81695ad7de
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2026-01-16 12:46:49 +0100

    Update test_button.py

--- test_button.py
+++ test_button.py
@@ -7,8 +7,8 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-# Pass number 1 to the callback using lambda
-GPIO.add_event_detect(10, GPIO.RISING, callback=lambda channel: button_callback(channel, 1)) # Setup event on pin 10 rising edge
+# Pass number 1 to the callback using lambda, with 200ms debounce time
+GPIO.add_event_detect(10, GPIO.RISING, callback=lambda channel: button_callback(channel, 1), bouncetime=200) # Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
