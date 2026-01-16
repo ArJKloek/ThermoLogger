45394d3f89564e07ca6882711944b24a99d8dd2b
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2026-01-16 12:43:42 +0100

    Pass button number to callback in GPIO event
    Modified the button_callback function to accept a button number and updated the GPIO event detection to use a lambda, passing the number 1. This allows the callback to identify which button was pressed, improving flexibility for handling multiple buttons.

--- test_button.py
+++ test_button.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def button_callback(channel):
-    print("Button was pushed!")
+def button_callback(channel, number):
+    print(f"Button {number} was pushed!")
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+# Pass number 1 to the callback using lambda
+GPIO.add_event_detect(10, GPIO.RISING, callback=lambda channel: button_callback(channel, 1)) # Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
