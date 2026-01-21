3d80ef1f81b07b4cf2ff1685df77d2e2b49c5181
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2026-01-21 11:23:03 +0100

    Update test_button.py

--- test_button.py
+++ test_button.py
@@ -7,13 +7,13 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 13 to be an input pin and set initial value to be pulled low (off)
-#GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 to be an input pin and set initial value to be pulled low (off)
-#GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 36 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 15 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 31 to be an input pin and set initial value to be pulled low (off)
 # Pass number 1 to the callback using lambda, with 200ms debounce time
 GPIO.add_event_detect(16, GPIO.RISING, callback=lambda channel: button_callback(channel, 1), bouncetime=200) # Setup event on pin 16 rising edge
 GPIO.add_event_detect(13, GPIO.RISING, callback=lambda channel: button_callback(channel, 2), bouncetime=200) # Setup event on pin 13 rising edge
-#GPIO.add_event_detect(16, GPIO.RISING, callback=lambda channel: button_callback(channel, 3), bouncetime=200) # Setup event on pin 16 rising edge
-#GPIO.add_event_detect(36, GPIO.RISING, callback=lambda channel: button_callback(channel, 4), bouncetime=200) # Setup event on pin 36 rising edge
+GPIO.add_event_detect(15, GPIO.RISING, callback=lambda channel: button_callback(channel, 3), bouncetime=200) # Setup event on pin 16 rising edge
+GPIO.add_event_detect(31, GPIO.RISING, callback=lambda channel: button_callback(channel, 4), bouncetime=200) # Setup event on pin 31 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
